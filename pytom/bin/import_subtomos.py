8aebcf29222e96374b72c8242435c91f250a4749
McHaillet <martenchaillet@gmail.com>
2023-03-14 12:26:45 +0100

    use convert.py for converting star 2 xml when importing submtomos

--- pytom/bin/import_subtomos.py
+++ pytom/bin/import_subtomos.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env pytom
 
-import sys, os
+import sys
+import os
 import multiprocessing
 from pytom.tools.script_helper import ScriptHelper2, ScriptOption2
 from pytom.tools.parse_script_options import parse_script_options2
-from pytom.agnostic.io import read_star, read, write, read_pixelsize
-from pytom.basic.structures import ParticleList, Particle, PickPosition, Rotation, Shift, Wedge
-from pytom.basic.score import FLCFScore
-from pytom.agnostic.tools import convert_angles
+from pytom.agnostic.io import read, write, read_pixelsize
+from pytom.basic.structures import ParticleList
 from pytom.agnostic.transform import fourier_reduced2full, fourier_full2reduced
 from functools import partial
 
@@ -34,8 +33,8 @@ def convert_subtomos(pl, invert_contrast=False):
         new_ctf_file = os.path.join(folder, basename + '_pytom' + ext)
         pixel_size = read_pixelsize(ctf_file)
         ctf = read(ctf_file)
-        ctf[ctf < 0] = 0
-        ctf[ctf > 1] = 1
+        # ctf[ctf < 0] = 0
+        # ctf[ctf > 1] = 1
         full_ctf = fourier_reduced2full(ctf, reduced_axis=0, isodd=ctf.shape[1] % 2)
         red_ctf = fourier_full2reduced(full_ctf, reduced_axis=2)
         write(new_ctf_file, red_ctf, pixel_size=pixel_size)
@@ -44,57 +43,11 @@ def convert_subtomos(pl, invert_contrast=False):
 
 def import_subtomos(star_file, xml_file, invert_contrast=False, n_cores=8):
     # read star only handles relion3.0/3.1 currently
-    stardata = read_star(star_file)
+    target, file_name = os.path.split(xml_file)
+    os.system(f"convert.py -f {star_file} -o xml -t "
+              f"{target if target != '' else './'} --outname {file_name.strip('.xml')}")
     particle_list = ParticleList()
-
-    for n in range(len(stardata['CoordinateX'])):
-        # set filename
-        p = Particle(filename=stardata['ImageName'][n])
-
-        # ===== pick position in tomogram
-        p.setPickPosition(PickPosition(stardata['CoordinateX'][n],
-                                       stardata['CoordinateY'][n],
-                                       stardata['CoordinateZ'][n],
-                                       originFilename=stardata['MicrographName'][n]))
-
-        # ======= set shifts
-        if 'OriginXAngst' in stardata.dtype.names:
-            star_pixel_size = stardata['PixelSize'][n]
-            x_shift, y_shift, z_shift = stardata['OriginXAngst'][n], stardata['OriginYAngst'][n], \
-                                        stardata['OriginZAngst'][n]
-
-            p.setShift(Shift(x=x_shift / star_pixel_size, y=y_shift / star_pixel_size, z=z_shift / star_pixel_size))
-
-            # factor = binningWarpM / binningPyTom
-            # p.getShift().setX(x * factor)
-            # p.getShift().setY(y * factor)
-            # p.getShift().setZ(z * factor)
-
-        # ======= class info from relion
-        if 'GroupNumber' in stardata.dtype.names:
-            p.setClass(stardata['GroupNumber'][n])
-
-        # ======= convert and set rotation
-        # relion uses internal axis, while pytom external, or the other way around
-        # anyway, need to invert the values
-        z0 = -stardata['AngleRot'][n]
-        y = -stardata['AngleTilt'][n]
-        z1 = -stardata['AnglePsi'][n]
-
-        z0, x, z1 = convert_angles((z0, y, z1), rotation_order='zyz', return_order='zxz')
-
-        p.setRotation(Rotation(z1=z0, x=x, z2=z1, paradigm='ZXZ'))
-
-        # ====== set wedge angles
-        # need to update the 3d ctf so that pytom can use it
-        # this means going reduced2full fourier space in X, and then full2reduced in Z
-        ctf_file = stardata['CtfImage'][n]
-        p.setWedge(Wedge(wedge_3d_ctf_file=ctf_file))
-
-        # === give particle empty score
-        p.setScore(FLCFScore())
-
-        particle_list.append(p)
+    particle_list.fromXMLFile(xml_file)
 
     print('each process will print some output about its progress')
     print('total number of particles to loop over: ', len(particle_list))
